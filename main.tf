c8b096244832d1c02cd35fec946d922f58b3eeb7
Martín Aguilar <tello@072004.xyz>
2025-10-15 21:51:22 -0600

    ajustes menores para despliegue exitoso

--- main.tf
+++ main.tf
@@ -222,7 +222,7 @@ resource "aws_instance" "frontend" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.default.key_name
   user_data = templatefile("${path.module}/config/frontend", {
-    backend_host = aws_instance.backend.public_ip
+    backend_host = aws_instance.backend.private_ip
   })
 
   tags = {
@@ -231,12 +231,11 @@ resource "aws_instance" "frontend" {
 }
 
 resource "aws_instance" "backend" {
-  ami                         = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
-  instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.private[0].id
-  vpc_security_group_ids      = [aws_security_group.backend.id]
-  associate_public_ip_address = false
-  key_name                    = aws_key_pair.default.key_name
+  ami                    = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.backend.id]
+  key_name               = aws_key_pair.default.key_name
   user_data = templatefile("${path.module}/config/backend", {
     db_host     = aws_db_instance.database.address
     db_port     = aws_db_instance.database.port
